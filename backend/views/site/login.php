22f27d667f087560d636cec0ff0de61a83e2d78e
nuhalinaatqia <nuhalinatqia@gmail.com>
2022-02-13 22:06:12 +0700

    integrated with sb admin 2

---
+++ backend/views/site/login.php
@@ -0,0 +1,70 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap4\ActiveForm */
+/* @var $model \common\models\LoginForm */
+
+use yii\bootstrap4\ActiveForm;
+use yii\bootstrap4\Html;
+use yii\helpers\Url;
+
+$this->title = 'Login';
+?>
+<!-- <div class="site-login">
+    <div class="mt-5 offset-lg-3 col-lg-6">
+        <h1><?= Html::encode($this->title) ?></h1>
+
+        <p>Please fill out the following fields to login:</p>
+
+        <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+
+        <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+
+        <?= $form->field($model, 'password')->passwordInput() ?>
+
+        <?= $form->field($model, 'rememberMe')->checkbox() ?>
+
+        <div class="form-group">
+            <?= Html::submitButton('Login', ['class' => 'btn btn-primary btn-block', 'name' => 'login-button']) ?>
+        </div>
+
+        <?php ActiveForm::end(); ?>
+    </div>
+</div> -->
+
+<div class="row">
+    <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+    <div class="col-lg-6">
+        <div class="p-5">
+            <div class="text-center">
+                <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+            </div>
+            <?php $form = ActiveForm::begin([
+                'id' => 'login-form',
+                'options' => ['class' => 'user']
+            ]); ?>
+
+            <?= $form->field($model, 'username', [
+                'inputOptions' => [
+                    'class' => 'form-control form-control-user',
+                    'placeholder' => 'Enter your username',
+                ]
+            ])->textInput(['autofocus' => true]) ?>
+
+            <?= $form->field($model, 'password', [
+                'inputOptions' => [
+                    'class' => 'form-control form-control-user',
+                    'placeholder' => 'Enter your password',
+                ]
+            ])->passwordInput() ?>
+
+            <?= $form->field($model, 'rememberMe')->checkbox() ?>
+            <?= Html::submitButton('Login', ['class' => 'btn btn-primary btn-user btn-block', 'name' => 'login-button']) ?>
+            <?php ActiveForm::end() ?>
+            <hr>
+            <div class="text-center">
+                <a class="small" href="<?php echo \yii\helpers\Url::to(['/site/forgot-password']) ?>">Forgot Password?</a>
+            </div>
+        </div>
+    </div>
+</div>
